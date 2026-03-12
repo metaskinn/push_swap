a124332b2eff8e8c4670662f6ebe014525d22bed
alex44 <sergeyevnasa@gmail.com>
2026-03-13 02:38:58 +0300

    fix: split

--- Makefile
+++ Makefile
@@ -2,22 +2,32 @@ NAME = push_swap
 
 CC = cc
 
-CFLAGS = -Wall -Wextra -Werror
+CFLAGS = -Wall -Wextra -Werror -I include/
+
+
+
+SRC_DIR = src
+OBJ_DIR = obj
+
+
 
 SRCS = \
-	src/main.c \
-	src/parser/split.c \
-	src/helpers/atol.c
-
-OBJS = $(SRCS:.c=.o)
+	${PATH}/main.c \
+	${PATH}/parser/split.c \
+	${PATH}/parser/atol.c\
+	${PATH}/parser/ft_libft.c
+	
+OBJS = $(SRCS:$(SRC_DIR)/%.c=$(OBJ_DIR)/%.o)
 
 all: ${NAME}
 
 ${NAME} : ${OBJS}
 	${CC} ${CFLAGS} ${OBJS} -o ${NAME}
 
+$(OBJ_DIR)/%.o: $(SRC_DIR)/%.c
+	@mkdir -p $(dir $@)
 clean:
-	rm -rf ${OBJS}
+	rm -rf ${OBJ_DIR}
 
 fclean: clean
 	rm -rf ${NAME}
--- include/push_swap.h
+++ include/push_swap.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   push_swap.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: metaskin <metaskin@student.42istanbul.com.t+#+  +:+       +#+        */
+/*   By: asobolev <asobolev@student.42istanbul.com.t+#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/07 23:50:34 by metaskin          #+#    #+#             */
-/*   Updated: 2026/03/08 09:57:48 by metaskin         ###   ########.fr       */
+/*   Updated: 2026/03/13 00:31:38 by asobolev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,22 @@
 # include <stdio.h>
 # include <unistd.h>
 
+
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+char	**ft_split(char const *s);
 char	**splitt(char *s);
-void	free_split(char **arr);
+char	*ft_strdup(const char *s);
+
 int		error(void);
 
+long	ft_atol(char *str);
+
+int 	is_wspace(char c);
+
+size_t	ft_strlen(const char *s);
+
+void	free_split(char **arr);
+void	*ft_memset(void *b, int c, size_t n);
 
 
 #endif
