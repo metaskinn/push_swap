49c9b5ee097a199b69a770e6d24fcbb303939ec7
alex44 <sergeyevnasa@gmail.com>
2026-03-14 00:21:11 +0300

    ilk hata kontrolleri

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: asobolev <asobolev@student.42istanbul.com.t+#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/08 00:22:26 by metaskin          #+#    #+#             */
-/*   Updated: 2026/03/14 00:11:46 by asobolev         ###   ########.fr       */
+/*   Updated: 2026/03/14 00:19:48 by asobolev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,12 +16,14 @@ int	main(int argc, char **argv)
 	if (argc < 2)
 		return (0);
 	return (0);
+}
 #include "push_swap.h"
+
 
 int main()
 {
    int i=0;
-   int atol;
+   int number;
    char *strng = "   21 44+ 55 86";
 
     char **splitend =splitt(strng);
@@ -29,8 +31,12 @@ int main()
     while(splitend[i]!=NULL)
     {
 
-        if (is_int(atol) == 0)
+        if (is_digit(splitend[i])== 0)
            error();
+        
+        number = ft_atol(splitend[i]);
+        if (is_int(number) == 0)
+            error();
         i++;
     }
     
