6cbe7a63c796761e98bed2f2a0fcd53ab872644c
metaskin <taskinmeltem04@gmail.com>
2026-03-14 06:17:05 +0300

    refactor: rename input parsing and algo flag helpers

--- include/parser.h
+++ include/parser.h
@@ -6,7 +6,7 @@
 /*   By: metaskin <metaskin@student.42istanbul.com.t+#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/14 17:05:00 by metaskin          #+#    #+#             */
-/*   Updated: 2026/03/14 04:40:49 by metaskin         ###   ########.fr       */
+/*   Updated: 2026/03/14 06:10:35 by metaskin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ typedef struct s_parse
 	int		need_free;
 }			t_parse;
 
-int			parse_input(int argc, char **argv, t_flags *flag, t_parse *parse);
+int			args_input(int argc, char **argv, t_flags *flag, t_parse *parse);
 char		**split(char *s);
 long		ft_atol(char *str);
 int			is_wspace(char c);
--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: metaskin <metaskin@student.42istanbul.com.t+#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/08 00:22:26 by metaskin          #+#    #+#             */
-/*   Updated: 2026/03/14 04:33:15 by metaskin         ###   ########.fr       */
+/*   Updated: 2026/03/14 06:10:40 by metaskin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ int	main(int argc, char **argv)
 	t_flags	flag;
 	t_parse	parse;
 
-	if (parse_input(argc, argv, &flag, &parse) == 0)
+	if (args_input(argc, argv, &flag, &parse) == 0)
 		return (0);
 	(void)flag;
 	if (parse.need_free)
