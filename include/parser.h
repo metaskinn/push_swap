a3684eaf22b09f9c60cd377119850ac22e9e9325
metaskin <taskinmeltem04@gmail.com>
2026-03-14 04:46:37 +0300

    Refactors parsing flow around a shared context
    Improves input handling by grouping related parsing state into one structure.
    Reduces pointer juggling and makes ownership rules clearer, which lowers cleanup risk and makes later parser extensions safer.

--- include/parser.h
+++ include/parser.h
@@ -1,26 +1,30 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   parser.h                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: metaskin <metaskin@student.42istanbul.com.t+#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/03/08 00:22:26 by metaskin          #+#    #+#             */
-/*   Updated: 2026/03/14 02:30:10 by metaskin         ###   ########.fr       */
+/*   Created: 2026/03/14 17:05:00 by metaskin          #+#    #+#             */
+/*   Updated: 2026/03/14 04:40:49 by metaskin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "push_swap.h"
+#ifndef PARSER_H
+# define PARSER_H
 
-int	main(int argc, char **argv)
+# include "bench.h"
+
+typedef struct s_parse
 {
-	t_flags	flag;
-	t_parse	parse;
+	char	**args;
+	int		need_free;
+}			t_parse;
 
-	if (parse_input(argc, argv, &flag, &parse) == 0)
-		return (0);
-	(void)flag;
-	if (parse.need_free)
-		free_split(parse.args);
-	return (0);
-}
+int			parse_input(int argc, char **argv, t_flags *flag, t_parse *parse);
+char		**split(char *s);
+long		ft_atol(char *str);
+int			is_wspace(char c);
+void		free_split(char **arr);
+
+#endif
--- include/push_swap.h
+++ include/push_swap.h
@@ -6,42 +6,18 @@
 /*   By: metaskin <metaskin@student.42istanbul.com.t+#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/07 23:50:34 by metaskin          #+#    #+#             */
-/*   Updated: 2026/03/14 02:34:29 by metaskin         ###   ########.fr       */
+/*   Updated: 2026/03/14 04:38:48 by metaskin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PUSH_SWAP_H
 # define PUSH_SWAP_H
 
-# include "stack.h"
 # include "bench.h"
+# include "parser.h"
+# include "stack.h"
 # include "utils.h"
-
-# include <stdio.h>
 # include <stdlib.h>
 # include <unistd.h>
-
-char	*ft_substr(char const *s, unsigned int start, size_t len);
-char	**split(char *s);
-char	*ft_strdup(const char *s);
-int		parse_input(int argc, char **argv, t_flags *flag,
-			char ***args, int *need_free);
-
-int		error(void);
-int		arr_len(char **s);
-int		has_dup(char **args);
-
-long	ft_atol(char *str);
-
-int		is_wspace(char c);
-int		is_intrange(long num);
-int		is_numstr(char *str);
-
-size_t	ft_strlen(const char *s);
-
-void	free_split(char **arr);
-void	*ft_memset(void *b, int c, size_t n);
-void	*ft_calloc(size_t count, size_t size);
-int	mt_strcmp(const char *s1, const char *s2);
 
 #endif
