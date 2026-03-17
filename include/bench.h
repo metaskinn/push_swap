b99c3c463244f5dda7facfb9b5cf290b9f65b061
metaskinn <taskinmeltem04@gmail.com>
2026-03-17 18:23:43 +0300

    feat(bench): add benchmark flag parsing helpers

--- include/bench.h
+++ include/bench.h
@@ -6,7 +6,7 @@
 /*   By: metaskin <metaskin@student.42istanbul.com.t+#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/14 01:48:15 by metaskin          #+#    #+#             */
-/*   Updated: 2026/03/17 16:28:01 by metaskin         ###   ########.fr       */
+/*   Updated: 2026/03/17 17:11:02 by metaskin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,5 +20,15 @@ typedef enum e_which_algo
 	MEDIUM,
 	COMPLEX
 }					t_which_algo;
+
+typedef struct s_flags
+{
+	t_which_algo	which_algo;
+	int				bench;
+	int				algo_did;
+}					t_flags;
+
+int	are_flags(char *arg);
+int	apply_flag(t_flags *flag, char *arg);
 
 #endif
