09ee9aa8d8f6ed51dd8c49a06332fb1d99349a73
metaskin <taskinmeltem04@gmail.com>
2026-03-17 16:29:48 +0300

    fix: remove duplicate main and add benchmark algo enum

--- include/bench.h
+++ include/bench.h
@@ -6,12 +6,19 @@
 /*   By: metaskin <metaskin@student.42istanbul.com.t+#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/14 01:48:15 by metaskin          #+#    #+#             */
-/*   Updated: 2026/03/15 17:41:39 by metaskin         ###   ########.fr       */
+/*   Updated: 2026/03/17 16:28:01 by metaskin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef BENCH_H
 # define BENCH_H
 
+typedef enum e_which_algo
+{
+	ADAPTIVE,
+	SIMPLE,
+	MEDIUM,
+	COMPLEX
+}					t_which_algo;
 
 #endif
