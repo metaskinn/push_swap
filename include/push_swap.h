4efc028c14b87f32d32ce273827fbd80098033ba
metaskinn <taskinmeltem04@gmail.com>
2026-03-17 18:28:21 +0300

    refactor: use t_program and read_args in main

--- include/push_swap.h
+++ include/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: metaskin <metaskin@student.42istanbul.com.t+#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/07 23:50:34 by metaskin          #+#    #+#             */
-/*   Updated: 2026/03/17 18:22:01 by metaskin         ###   ########.fr       */
+/*   Updated: 2026/03/17 18:25:23 by metaskin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,14 +19,15 @@
 # include "utils.h"
 # include <stdlib.h>
 # include <unistd.h>
+# include <algo.h>
 
-typedef struct s_main
+typedef struct s_program
 {
 	t_stack	a;
 	t_stack	b;
 	t_flags	flag;
 	char	**args;
-	
-}			t_main;
+
+}			t_program;
 
 #endif
--- src/main.c
+++ src/main.c
@@ -6,42 +6,43 @@
 /*   By: metaskin <metaskin@student.42istanbul.com.t+#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/08 00:22:26 by metaskin          #+#    #+#             */
-/*   Updated: 2026/03/17 18:16:22 by metaskin         ###   ########.fr       */
+/*   Updated: 2026/03/17 18:26:35 by metaskin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-int	main(int argc, char **argv)
-{
-	char	**splited;
-	int i;
-	long atoled;
+// int	main(int argc, char **argv)
+// {
+// 	char	**splited;
+// 	int i;
+// 	long atoled;
 
-	i = 0;
+// 	i = 0;
 
-	if (argc == 2)
-		splited = split(argv[1]);
-	else
-		splited = argv + 1;
+// 	if (argc == 2)
+// 		splited = split(argv[1]);
+// 	else
+// 		splited = argv + 1;
 
-	while (splited[i] != NULL)
-{
-    is_numstr(splited[i]);
-    atoled = ft_atol(splited[i]);
-    is_intranger_range(atoled);
-    i++;
-}
-	has_dup(splited);
-	return (0);
-}
+// 	while (splited[i] != NULL)
+// {
+//     is_numstr(splited[i]);
+//     atoled = ft_atol(splited[i]);
+//     is_intranger_range(atoled);
+//     i++;
+// }
+// 	has_dup(splited);
+// 	return (0);
+// }
 
-int	main(int argc, char **argv)
+int main(int argc, char **argv)
 {
 	t_program	program;
 
-	if (read_args( argc, argv,))
-		return (0);
+	if (!read_args(argc, argv, &program.flag))
+		error();
+	program.args = argv;
 
 	return (0);
 }
