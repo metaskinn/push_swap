ce6a6978337174d4fd2705b0d17016f2b18231c2
asobolev <sergeyevnasa@gmail.com>
2026-03-17 15:18:53 +0300

    chore: Running git to list staged files to craft an accurate conventiona

--- src/main.c
+++ src/main.c
@@ -6,10 +6,33 @@
 /*   By: asobolev <asobolev@student.42istanbul.com.t+#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/08 00:22:26 by metaskin          #+#    #+#             */
-/*   Updated: 2026/03/17 15:05:26 by asobolev         ###   ########.fr       */
+/*   Updated: 2026/03/17 15:07:41 by asobolev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "push_swap.h"
+
+int	main(int argc, char **argv)
+{
+	char	**splited;
+	int i;
+	int atoled;
+	
+	i = 0;
+	
+	if (argc == 2)
+		splited = split(argv[1]);
+	else
+		*splited = argv[i];	
+	while (splited[i] != NULL)
+	{
+		is_number(splited[i]);
+		atoled = is_intrager_range(ft_atol(splited[i]));
+		i++;
+	}
+	has_dup(ft_atol(splited));
+	return (0);
+}
 #include "push_swap.h"
 
 int	main(int argc, char **argv)
