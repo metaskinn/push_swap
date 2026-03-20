ed8b9a3928193cf67ca1eb3086f91aeb80804fd9
metaskin <taskinmeltem04@gmail.com>
2026-03-20 15:57:11 +0300

    chore: reorganize ops files and clean header formatting

--- include/bench.h
+++ include/bench.h
@@ -6,7 +6,7 @@
 /*   By: metaskin <metaskin@student.42istanbul.com.t+#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/14 01:48:15 by metaskin          #+#    #+#             */
-/*   Updated: 2026/03/17 18:21:26 by metaskin         ###   ########.fr       */
+/*   Updated: 2026/03/20 15:42:08 by metaskin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ typedef enum e_which_algo
 	SIMPLE,
 	MEDIUM,
 	COMPLEX
-}			t_which_algo;
+}					t_which_algo;
 
 typedef struct s_flags
 {
@@ -29,12 +29,12 @@ typedef struct s_flags
 	int				total_ops;
 	int				enabled;
 	int				disorder;
-}		t_flags;
+}					t_flags;
 
-
-void	default_flag(t_flags *flag);
-int		are_flags(char *arg);
-int		apply_flag(t_flags *flag, char *arg);
-int		handle_flags(int argc, char **argv, t_flags *flag, int *start);
+void				default_flag(t_flags *flag);
+int					are_flags(char *arg);
+int					apply_flag(t_flags *flag, char *arg);
+int					handle_flags(int argc, char **argv, t_flags *flag,
+						int *start);
 
 #endif
