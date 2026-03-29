dab911b917c22da4b5bdf11161ab4a5a8d8312c2
metaskin <taskinmeltem04@gmail.com>
2026-03-29 05:44:16 +0300

    fix: implement chunk-based medium sort algorithm

--- src/algorithms/selector.c
+++ src/algorithms/selector.c
@@ -6,7 +6,7 @@
 /*   By: metaskin <metaskin@student.42istanbul.com.t+#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/25 07:35:00 by metaskin          #+#    #+#             */
-/*   Updated: 2026/03/25 07:41:44 by metaskin         ###   ########.fr       */
+/*   Updated: 2026/03/28 19:59:34 by metaskin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,5 +22,5 @@ void	run_selected_algo(t_program *program)
 	else if (program->flag.which_algo == COMPLEX)
 		complex_sort(&program->a, &program->b, &program->flag);
 	else
-		simple_sort(&program->a, &program->b, &program->flag);
+		medium_sort(&program->a, &program->b, &program->flag);
 }
