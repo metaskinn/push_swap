328ed021ad66e98f0c434405cf61e71a33504c61
metaskinn <taskinmeltem04@gmail.com>
2026-04-02 12:48:49 +0300

    chore: remove unused includes from headers

--- include/algo.h
+++ include/algo.h
@@ -6,17 +6,14 @@
 /*   By: metaskin <metaskin@student.42istanbul.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/17 18:22:57 by metaskin          #+#    #+#             */
-/*   Updated: 2026/04/02 02:46:03 by metaskin         ###   ########.fr       */
+/*   Updated: 2026/04/02 12:47:05 by metaskin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef ALGO_H
 # define ALGO_H
 
-# include "parser.h"
 # include "push_swap.h"
-# include "stack.h"
-# include <stdlib.h>
 
 double	compute_disorder(t_stack head);
 void	simple_sort(t_stack *a, t_stack *b, t_flags *flag);
