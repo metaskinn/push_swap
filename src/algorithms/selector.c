371a96b820c9299d18c37ff40fe4946c93a3b1e9
metaskin <taskinmeltem04@gmail.com>
2026-04-02 12:57:40 +0300

    chore: remove adaptive sort handling

--- src/algorithms/selector.c
+++ src/algorithms/selector.c
@@ -6,7 +6,7 @@
 /*   By: metaskin <metaskin@student.42istanbul.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/25 07:35:00 by metaskin          #+#    #+#             */
-/*   Updated: 2026/04/02 12:44:07 by metaskin         ###   ########.fr       */
+/*   Updated: 2026/04/02 12:54:07 by metaskin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,8 +20,6 @@ void	run_selected_algo(t_program *program)
 		medium_sort(&program->a, &program->b, &program->flag);
 	else if (program->flag.which_algo == COMPLEX)
 		complex_sort(&program->a, &program->b, &program->flag);
-	else if (program->flag.which_algo == ADAPTIVE)
-		adaptive_sort(&program->a, &program->b, &program->flag);
 	else
 		adaptive_sort(&program->a, &program->b, &program->flag);
 }
