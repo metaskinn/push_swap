8986fde9b256c2abc473f7811ed2a04972cbfe8e
metaskinn <taskinmeltem04@gmail.com>
2026-04-02 03:04:47 +0300

    fix: add ft_printf implementation

--- src/utils/second_func.c
+++ src/utils/second_func.c
@@ -6,7 +6,7 @@
 /*   By: metaskin <metaskin@student.42istanbul.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/14 16:35:00 by metaskin          #+#    #+#             */
-/*   Updated: 2026/04/02 02:06:15 by metaskin         ###   ########.fr       */
+/*   Updated: 2026/04/02 03:00:45 by metaskin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,4 +20,9 @@ int	ft_strcmp(const char *s1, const char *s2)
 	while (s1[idx] && s2[idx] && s1[idx] == s2[idx])
 		idx++;
 	return ((unsigned char)s1[idx] - (unsigned char)s2[idx]);
+}
+
+int	ft_putchar(char c)
+{
+	return (write(1, &c, 1));
 }
